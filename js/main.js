fd2f82027d8148553fe71b948389068423bfbdb5
nahidhasan141400 <nahidhasan141400@gmail.com>
2023-12-13 12:02:36 +0600

    🚀 add lazy loading to the image loading

--- js/main.js
+++ js/main.js
@@ -1,22 +1,40 @@
 const cureor = document.querySelector(".cursor");
 const cureorDot = document.querySelector(".cursor-dot");
 
-window.addEventListener("mousemove",(e)=>{
-    let x = e.clientX;
-    let y = e.clientY;
-    cureor.style.left = `${x}px`
-    cureor.style.top = `${y}px`
-    cureorDot.style.left = `${x}px`
-    cureorDot.style.top = `${y}px`
-})
+window.addEventListener("mousemove", (e) => {
+  let x = e.clientX;
+  let y = e.clientY;
+  cureor.style.left = `${x}px`;
+  cureor.style.top = `${y}px`;
+  cureorDot.style.left = `${x}px`;
+  cureorDot.style.top = `${y}px`;
+});
 
-const clink = document.querySelectorAll('.clink');
+const clink = document.querySelectorAll(".clink");
 
-clink.forEach((e)=>{
-    e.addEventListener('mouseenter',()=>{
-        cureor.classList.add("canim")
-    })
-    e.addEventListener('mouseleave',()=>{
-        cureor.classList.remove("canim")
-    })
-})
+clink.forEach((e) => {
+  e.addEventListener("mouseenter", () => {
+    cureor.classList.add("canim");
+  });
+  e.addEventListener("mouseleave", () => {
+    cureor.classList.remove("canim");
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const lazyImages = document.querySelectorAll(".lazy-load");
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        const lazyImage = entry.target;
+        lazyImage.src = lazyImage.dataset.src;
+        observer.unobserve(lazyImage);
+      }
+    });
+  });
+
+  lazyImages.forEach((image) => {
+    observer.observe(image);
+  });
+});
